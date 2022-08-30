4a69072a412c0c82712cb82aad10c90d36dd3f4e
Vladislav Fateev <Fateev_wbd@mail.ru>
2022-08-30 22:41:13 +0500

    30.08

--- src/component/Main/Dialogs/Dialogs.jsx
+++ src/component/Main/Dialogs/Dialogs.jsx
@@ -3,7 +3,20 @@ import DialogsClass from './Dialogs.module.css'
 const Dialogs = () => {
     return(
         <div className={DialogsClass.content}>
-            Dialogs
+            <h2 className={DialogsClass.title}>Dialogs</h2>
+            <div className={DialogsClass.dialogs}>
+                <div className={DialogsClass.dialogItem}>
+                    <div className={DialogsClass.item}>Yosya</div>
+                    <div className={DialogsClass.item}>Vasa</div>
+                    <div className={DialogsClass.item}>Babich</div>
+                    <div className={DialogsClass.item}>Sirock</div>
+                    <div className={DialogsClass.item}>Holodock</div>
+                </div>
+            <div className={DialogsClass.messages}>
+                <div className={DialogsClass.message}>Yo</div>
+                <div className={DialogsClass.message}>How are you</div>
+                <div className={DialogsClass.message}>My name is Yosya</div>
+            </div>
         </div>
     )
 }
